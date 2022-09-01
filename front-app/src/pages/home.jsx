9f3bc3429c54d81d7b7f86cbd514c237d4ce8525
Gabriel <gabriel.gabrielyea@gmail.com>
2022-09-01 17:01:52 -0500

    chores(linter): add linters config

---
+++ front-app/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function home() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>home</div>
+    </div>
+  );
+}
